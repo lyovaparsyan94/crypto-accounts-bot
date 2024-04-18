8b91afd26b28dd963dd216dc8cf66bf0daf6d9fa
Lyova PArsyan <parsyan.lyova@gmail.com>
2024-04-19 03:01:03 +0400

    headless

--- src/aws_scrapper.py
+++ src/aws_scrapper.py
@@ -31,7 +31,7 @@ class BaseRegistrator:
 
     def create_driver(self):
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
